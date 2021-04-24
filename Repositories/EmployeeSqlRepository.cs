de9b648021050dcb5b8f24a37cdcc0b0eab1ffab
vtchris <vtchris.2357@gmail.com>
2021-04-24 15:20:23 -0400

    56: Adding and implementing interface

---
+++ Repositories/EmployeeSqlRepository.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManager.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.API.Repositories
+{
+    public class EmployeeSqlRepository : IEmployeeRepository
+    {
+        public void Delete(Employee emp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Employee emp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee SelectById(Employee emp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employee emp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
